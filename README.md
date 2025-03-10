815ac3e6c0c1db3bc832b7a355e9156b70e3beed
berracode <juanguillermoalarcon@gmail.com>
2025-03-09 22:35:31 -0500

    se agregan estilos al sidebar y carga de pods

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This template should help get you started developing with Tauri, React and Typescript in Vite.
 
+## colors
+
+https://uicolors.app/generate/252628-447383-258eb1
+
+
+
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
--- src/ui/components/sidebar/Sidebar.tsx
+++ src/ui/components/sidebar/Sidebar.tsx
@@ -4,13 +4,25 @@ import { Namespace } from '../../../types/Namespace';
 import { NamespaceController } from '../../../controllers/NamespaceController';
 import { FileService } from '../../../services/FileService';
 import { useNamespaceContext } from '../../context/namespace/NameSpaceContext';
+import podsData from '../../../data-mock/pod.json'; // mi mock de pods
+
 
 function Sidebar() {
   const { namespacesState: {namespaces}, refreshNamespaces } = useNamespaceContext();
+  const [pods, setPods] = useState<{ [namespace: string]: string[] }>({});
+  const [loadingPods, setLoadingPods] = useState<{ [namespace: string]: boolean }>({});
+  const [expanded, setExpanded] = useState<{ [namespace: string]: boolean }>({});
+
 
   const handleNamespaceClick = (namespace: Namespace) => {
     // Lógica para manejar el clic en un namespace
     console.log('Namespace clicked:', namespace);
+  };
+
+  const handleNamespaceClick2 = (namespace: Namespace) => {
+    console.log('Namespace clicked:', namespace);
+
+    setExpanded(prev => ({ ...prev, [namespace.nombre]: !prev[namespace.nombre] }));
   };
 
   const handleRefreshNs = () => {
@@ -19,18 +31,34 @@ function Sidebar() {
     refreshNamespaces()
   }
 
+  useEffect(() => {
+    setPods({})
+    console.log("data pods: "+JSON.stringify(podsData));
+    
+    // Cargar los pods en segundo plano
+    namespaces.forEach(ns => {
+      setLoadingPods(prev => ({ ...prev, [ns.nombre]: true }));
+      setTimeout(() => { // Simula la carga demorada
+        const namespacePods = podsData[ns.nombre] || []; 
+        setPods(prev => ({ ...prev, [ns.nombre]: namespacePods }));
+        setLoadingPods(prev => ({ ...prev, [ns.nombre]: false }));
+      }, 1500); // Simula un retraso de 1 segundo
+    });
+  }, [namespaces]);
+
   return (
     <aside style={{
       width: '256px', 
       display: 'flex',
       flexDirection: 'column',
-      background: '#333', 
+      //background: '#333', 
+      background: '#252628',
       color: 'white', 
-      border: '1px solid red', 
+      border: '0px solid red', 
       }}>
       <div
         style={{
-          border: '1px solid green',
+          border: '0px solid green',
           justifyContent: 'space-between',
           alignItems: 'center',
           display: 'flex',
@@ -38,31 +66,52 @@ function Sidebar() {
           padding: '10px'
         }}
       >
-        <text>Namespaces</text>
+        <text style={{fontWeight: 'bold'}}>Namespaces</text>
         <button onClick={handleRefreshNs}>Refresh</button>
       </div>
-
+      <span style={{borderBottom: '1px solid #333455'}}></span>
       <div style={{
-        border: '2px solid blue',
+        border: '0px solid blue',
         display: 'flex',
         flexDirection: 'column',
         height: '100%',
-        justifyContent: 'space-between',
-        overflowY: 'auto'
+        overflowY: 'auto',
 
       }}>
+       
+        <nav style={{border: '0px solid green'}}>
+          <ul style={{
+            display: 'flex', 
+            flexDirection: 'column',
+            gap: 10
+          }}>
+            {namespaces.map((ns, index) => (
+              <li
+                key={index}
+                style={{border: '0px solid red' }}
+              >
+                <div style={{display: 'flex', flexDirection: 'column'}} onClick={() => handleNamespaceClick2(ns)}>
+                  <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', border: '0px solid red'}}>
+                    <span>{ns.nombre}</span>
+                    <span style={{ marginRight: '0px' }}>
+                      {expanded[ns.nombre] ? '▼' : '▶'} {/* Icono de flecha */}
+                    </span>
+                  </div>
+                  {loadingPods[ns.nombre] && <span>Cargando pods...</span>}
+                </div>
 
-        <ul>
-          {namespaces.map((ns, index) => (
-            <li
-              key={index}
-              style={{ padding: '10px', cursor: 'pointer' }}
-              onClick={() => handleNamespaceClick(ns)}
-            >
-              {ns.nombre}
-            </li>
-          ))}
-        </ul>
+                {expanded[ns.nombre] && pods[ns.nombre] && (
+                  <ul>
+                    {pods[ns.nombre].map((pod, podIndex) => (
+                      <li key={podIndex}>{pod}</li>
+                    ))}
+                  </ul>
+                )}
+
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </aside>
   );
